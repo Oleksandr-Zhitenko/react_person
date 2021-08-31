639fbdc2e3ab68825daee3117afdadc3887019cc
Oleksandr Zhitenko <geetonio28@gmail.com>
2021-08-31 18:19:58 +0300

    Updates

---
+++ precache-manifest.fbf25d8fb2075b427bbb60200be68b67.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8a7fbc237ec95716db22d250a9d059a0",
+    "url": "./index.html"
+  },
+  {
+    "revision": "09cc35f13e7073e7dfeb",
+    "url": "./static/css/main.ae4f840b.chunk.css"
+  },
+  {
+    "revision": "bef14ca31350064182c6",
+    "url": "./static/js/2.e8d9cca5.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "./static/js/2.e8d9cca5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "09cc35f13e7073e7dfeb",
+    "url": "./static/js/main.b24740bf.chunk.js"
+  },
+  {
+    "revision": "e447b2ddaa3314191b11",
+    "url": "./static/js/runtime-main.e90a3edd.js"
+  }
+]);
